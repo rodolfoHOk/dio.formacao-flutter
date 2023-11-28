3b0dcd8563df1d887f5aa4a08edb2f19d4e1f1f4
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-27 22:58:13 -0300

    dart: tipos de dados e declaração de variáveis

--- dart/meu_app/bin/meu_app.dart
+++ dart/meu_app/bin/meu_app.dart
@@ -1,5 +1,19 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
-  print('Hello world - ${meu_app.calculate()}!');
+  String texto = "DIO";
+  var texto2 = "DIO";
+  String texto3;
+
+  int numero1 = 10;
+  var numero2 = 11;
+  int numero3;
+
+  List<String> lista = [];
+  lista.add("textoA");
+  // lista.add(1); // não permite
+  var lista2 = [];
+  lista2.add("textoA");
+  lista2.add(1);
+  lista2.add(1.2);
 }
