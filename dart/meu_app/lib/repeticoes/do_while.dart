fe7ecb233bd27aa4d82b157e58e28dabb48db4f5
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-28 15:33:38 -0300

    dart: repeticao do while

--- dart/meu_app/lib/repeticoes/do_while.dart
+++ dart/meu_app/lib/repeticoes/do_while.dart
@@ -1,11 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:meu_app/meu_app.dart' as meu_app;
-
 void main(List<String> arguments) {
-  print('Hello world - ${meu_app.calculate()}!');
-
   var opcao = "";
   var acumulador = 0.0;
 
