bf473dc57d1d47d5d42911e186098d5201209c4d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-28 12:10:04 -0300

    dart: tipos de dados const

---
+++ dart/meu_app/lib/tipos/const.dart
@@ -0,0 +1,11 @@
+void main(List<String> arguments) {
+  const String variavel = "ABC";
+  const int variavel1 = 1;
+  const bool variavel2 = true;
+
+  // variavel = "DEF"; // const não permite ser alterada
+
+  print(variavel);
+  print(variavel1);
+  print(variavel2);
+}
