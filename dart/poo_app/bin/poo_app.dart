3f0333053fd240066e92d0ccf43502ae7a2c792a
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-28 21:41:12 -0300

    dart: poo encapsulamento

--- dart/poo_app/bin/poo_app.dart
+++ dart/poo_app/bin/poo_app.dart
@@ -1,6 +1,9 @@
-import 'package:poo_app/classes/Pessoa.dart';
+import 'package:poo_app/classes/pessoa.dart';
 
 void main(List<String> arguments) {
   var p1 = Pessoa();
+  p1.setNome("Rodolfo");
+  p1.setEndereco("Rua 1, 1");
   print(p1);
+  print(p1.getNome());
 }
