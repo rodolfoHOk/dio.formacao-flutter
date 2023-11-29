409e0c68c84f3118a2b83f69d6be68e9edb18a5e
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-28 21:48:14 -0300

    dart: poo construtor

--- dart/poo_app/bin/poo_app.dart
+++ dart/poo_app/bin/poo_app.dart
@@ -1,8 +1,6 @@
 import 'package:poo_app/classes/pessoa.dart';
 
 void main(List<String> arguments) {
-  var p1 = Pessoa();
-  p1.setNome("Rodolfo");
-  p1.setEndereco("Rua 1, 1");
+  var p1 = Pessoa("Rodolfo", "Rua 1, 2");
   print(p1);
 }
--- dart/poo_app/lib/classes/pessoa.dart
+++ dart/poo_app/lib/classes/pessoa.dart
@@ -2,6 +2,11 @@ class Pessoa {
   String _nome = "";
   String _endereco = "";
 
+  Pessoa(String nome, String endereco) {
+    _nome = nome;
+    _endereco = endereco;
+  }
+
   void setNome(String nome) {
     _nome = nome;
   }
