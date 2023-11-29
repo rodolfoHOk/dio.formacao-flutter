e427bec821f59ac3da169d1c9570c3e76afb0948
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-28 21:08:44 -0300

    dart: funcoes arrow functions

---
+++ dart/meu_app/lib/funcoes/arrow_functions.dart
@@ -0,0 +1,20 @@
+void main(List<String> arguments) {
+  printData("Meu texto");
+  printData(soma(1, 2).toString());
+}
+
+// void printData(String texto) {
+//   print(texto);
+// }
+
+// printData(String texto) => print(texto);
+
+void printData(String texto) => print(texto);
+
+// int soma(int numero1, int numero2) {
+//   return numero1 + numero2;
+// }
+
+// soma(int numero1, int numero2) => numero1 + numero2;
+
+int soma(int numero1, int numero2) => numero1 + numero2;
