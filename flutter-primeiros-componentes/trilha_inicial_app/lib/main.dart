578059dfeae3301aec5ca99636091c6df9cb892f
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-30 10:55:08 -0300

    flutter: primeiros componentes gerando números aleatórios

--- flutter-primeiros-componentes/trilha_inicial_app/lib/main.dart
+++ flutter-primeiros-componentes/trilha_inicial_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,12 +29,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var generatedNumber = 0;
+
+  // void _generateRandomNumber() {
+  //   Random randomNumber = Random();
+  //   setState(() {
+  //     generatedNumber = randomNumber.nextInt(1000);
+  //   });
+  // }
+
+  int _generateRandomNumber() {
+    Random randomNumber = Random();
+    return randomNumber.nextInt(1000);
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint("chamando método build");
     return Scaffold(
       appBar: AppBar(
         title: const Text("Meu App"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(child: Text(generatedNumber.toString())),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red.shade300,
+        child: const Icon(Icons.add_reaction),
+        // onPressed: _generateRandomNumber,
+        onPressed: () {
+          setState(() {
+            generatedNumber = _generateRandomNumber();
+          });
+        },
       ),
     );
   }
