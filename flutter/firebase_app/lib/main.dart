aa6544720f8ef2b06a94d23a2d7dbb7d4c0d05e1
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-18 21:00:43 -0300

    flutter: firebase: remote config aplicacao

--- flutter/firebase_app/lib/main.dart
+++ flutter/firebase_app/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_app/main_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+final remoteConfig = FirebaseRemoteConfig.instance;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -14,17 +16,19 @@ void main() async {
   final remoteConfig = FirebaseRemoteConfig.instance;
   await remoteConfig.setConfigSettings(RemoteConfigSettings(
     fetchTimeout: const Duration(minutes: 1),
-    minimumFetchInterval: const Duration(hours: 1),
+    minimumFetchInterval: const Duration(minutes: 60),
   ));
   await remoteConfig.setDefaults(const {
     "example_param_1": 42,
     "example_param_2": 3.14159,
     "example_param_3": true,
     "example_param_4": "Hello, world!",
+    "CHAT_TEXT": "Informe seu apelido",
   });
   await remoteConfig.fetchAndActivate();
   var param_4 = remoteConfig.getString("example_param_4");
   debugPrint(param_4);
+  debugPrint(remoteConfig.getString("CHAT_TEXT"));
 
   runApp(const MainApp());
 }
--- flutter/firebase_app/lib/pages/home/home_page.dart
+++ flutter/firebase_app/lib/pages/home/home_page.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:firebase_app/pages/chat/chat_page.dart';
 import 'package:firebase_app/shared/widgets/custom_drawer.dart';
-import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,8 +11,13 @@ class HomePage extends StatelessWidget {
     var roomController = TextEditingController(text: "");
     var nicknameController = TextEditingController(text: "");
 
+    final remoteConfig = FirebaseRemoteConfig.instance;
+    debugPrint(remoteConfig.getString("CHAT_TEXT"));
+
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Color(int.parse(
+            "0xff${remoteConfig.getString("BACKGROUND_SCREEN_COLOR")}")),
         appBar: AppBar(
           title: const Text("Firebase App"),
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -28,7 +34,7 @@ class HomePage extends StatelessWidget {
                 controller: roomController,
               ),
               const SizedBox(height: 24),
-              const Text("Informe seu apelido"),
+              Text(remoteConfig.getString("CHAT_TEXT")),
               const SizedBox(height: 8),
               TextField(
                 controller: nicknameController,
